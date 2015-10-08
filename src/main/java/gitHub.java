139b81da7dfaf50595dff641eb89d25ddd5b968d
dfn <dongfangnan@shuzijiayuan.com>
2015-10-08 09:53:40 +0800

    gitRemote

---
+++ src/main/java/gitHub.java
@@ -0,0 +1,19 @@
+/**
+ * Created by dfn on 15/10/8.
+ */
+public class gitHub {
+
+    private String num;
+
+    public void getInfo(){
+
+        System.out.print( "test" );
+
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/gitList.java
@@ -0,0 +1,18 @@
+/**
+ * Created by dfn on 15/10/8.
+ */
+public class gitList {
+
+    /**
+     * 添加远程仓库
+
+     我在之前的章节中已经提到并展示了如何添加远程仓库的示例，不过这里将告诉你如何明确地做到这一点。
+     运行 git remote add <shortname> <url> 添加一个新的远程 Git 仓库，同时指定一个你可以轻松引用的简写：
+
+     $ git remote
+     origin
+     $ git remote add pb https://github.com/paulboone/ticgit
+     $ git remote -v
+     origin	https://github.com/schacon/ticgit (fetch)
+     */
+}
